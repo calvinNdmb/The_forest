8d81b595bc5772f2f27ea26d3a98db209381118c
Nagisa19 <clestrat@proton.me>
2024-12-19 10:48:14 +0100

    Fix merge

--- map/simulation_de_nuage.py
+++ map/simulation_de_nuage.py
@@ -1,11 +1,11 @@
 import pygame
 import numpy as np
-
-from map.nutrient_map import generate_nutrient_map
-from map.utils import create_noise
+import noise
 
 # Dimensions de la fenêtre
 width, height = 720, 640
+
+
 # Fonction de génération des clouds
 def generate_clouds_surface(scale, octaves, persistence, lacunarity, offset):
     nutrient_map = np.zeros((width, height))
@@ -13,7 +13,44 @@ def generate_clouds_surface(scale, octaves, persistence, lacunarity, offset):
         for j in range(height):
             nx = (i / scale) + offset
             ny = (j / scale) + offset
-            nutrient_map[i, j] = create_noise(nx=nx, ny=ny, octaves=octaves, persistence=persistence, lacunarity=lacunarity)
+            val = noise.pnoise2(nx, ny, octaves=octaves, persistence=persistence,
+                                lacunarity=lacunarity, repeatx=1024, repeaty=1024, base=5)
+            pixel_val = (val + 1) / 2 * 255
+            # Discrétisation
+            if pixel_val <= 50:
+                pixel_val = 0
+            elif pixel_val <= 100 and pixel_val > 50:
+                pixel_val = 0
+            elif pixel_val <= 150 and pixel_val > 100:
+                pixel_val = 0
+            elif pixel_val <= 200 and pixel_val > 150:
+                pixel_val = 255
+            nutrient_map[i, j] = pixel_val
+
+    nutrient_map_rgb = np.stack((nutrient_map, nutrient_map, nutrient_map), axis=-1).astype(np.uint8)
+    return pygame.surfarray.make_surface(nutrient_map_rgb)
+
+
+# Fonction de génération des nutriments
+def generate_nutrient_map(width, height, scale, octaves, persistence, lacunarity):
+    nutrient_map = np.zeros((width, height))
+    for i in range(width):
+        for j in range(height):
+            nx = i / scale
+            ny = j / scale
+            val = noise.pnoise2(nx, ny, octaves=octaves, persistence=persistence,
+                                lacunarity=lacunarity, repeatx=1024, repeaty=1024, base=9)
+            pixel_val = (val + 1) / 2 * 255
+            # Discrétisation
+            if pixel_val <= 50:
+                pixel_val = 0
+            elif pixel_val <= 100 and pixel_val > 50:
+                pixel_val = 100
+            elif pixel_val <= 150 and pixel_val > 100:
+                pixel_val = 150
+            elif pixel_val <= 200 and pixel_val > 150:
+                pixel_val = 255
+            nutrient_map[i, j] = pixel_val
     return nutrient_map
 
 
@@ -32,13 +69,13 @@ def main():
     offset = 1
 
     # Génération de la carte de nutriments ======================
-    nutrient_map = generate_nutrient_map(width, height,scale, octaves, persistence, lacunarity)
+    nutrient_map = generate_nutrient_map(width, height, scale, octaves, persistence, lacunarity)
     nutrient_map_rgb = np.stack(
-            (np.zeros_like(nutrient_map),  # canal rouge à 0
-            nutrient_map,                 # canal vert avec les nutriments
-            np.zeros_like(nutrient_map)), # canal bleu à 0
-            axis=-1
-        ).astype(np.uint8) # On convertit en entiers non-signés pour Pygame
+        (np.zeros_like(nutrient_map),  # canal rouge à 0
+         nutrient_map,  # canal vert avec les nutriments
+         np.zeros_like(nutrient_map)),  # canal bleu à 0
+        axis=-1
+    ).astype(np.uint8)  # On convertit en entiers non-signés pour Pygame
     nutrient_surf = pygame.surfarray.make_surface(nutrient_map_rgb)
 
     # Boucle principale ========================================
@@ -57,12 +94,10 @@ def main():
         screen.blit(nutrient_surf, (0, 0))
         screen.blit(nutrient_surface, (0, 0))
 
-
-
-
         pygame.display.flip()
         pygame.time.delay(1)
 
     pygame.quit()
+
 
 main()
